fd40f0b83769348be1e915a8b4687836af634af8
郑皓泽 <zhenghaoze666m@163.com>
2026-02-13 01:23:58 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+CanIRunIt package
+
+Tools for automatically judging how easy it is to understand and run a GitHub
+repository based on its README and basic project metadata.
+"""
+
+__all__ = ["__version__"]
+
+__version__ = "0.1.0"
+
